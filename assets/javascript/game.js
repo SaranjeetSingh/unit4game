8779c0fa008014f64a4e56dbf744bc7975bd7431
Saranjeet Singh <saranjeet.mca@gmail.com>
2018-10-02 16:53:17 -0400

    repetition in random number corrected.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,21 +2,28 @@ var sum = 0;
 var win = 0;
 var loss = 0;
 var random;
-var numberOptions = [10, 5, 3, 7];
-//var randomNumber;
+var a = [1,2,3,4,5,6,7,8,9,10];
+// var randomNumber;
 
 function onload(){
     sum = 0;
-    //randomNumber = Math.floor((Math.random() *10) + 1);
+    var r=[];
+    // randomNumber = Math.floor((Math.random() *10) + 1);
     //console.log(randomNumber);
+    for(var n=1;n<=4;++n){
+        var i = Math.floor((Math.random() * (10-n))+1);
+        r.push(a[i]);
+        a[i] = a[10-n];
+    }
+
     random = Math.floor((Math.random() * 80) + 30);
     console.log(random);
     $("#randomNumber").html(random);
     $("#total").text(sum);
-    $(".image1").attr("val",numberOptions[0]);
-    $(".image2").attr("val",numberOptions[1]);
-    $(".image3").attr("val",numberOptions[2]);
-    $(".image4").attr("val",numberOptions[3]);
+    $(".image1").attr("val",r[0]);
+    $(".image2").attr("val",r[1]);
+    $(".image3").attr("val",r[2]);
+    $(".image4").attr("val",r[3]);
 }
 
 $(document).ready(function() {
